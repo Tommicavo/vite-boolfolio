f5f6217490ff78610612cdeb52b5d36b24ae339a
Tommicavo <tommicava@icloud.com>
2023-09-13 16:18:06 +0200

    send mail from vue form

--- src/components/pages/ContactPage.vue
+++ src/components/pages/ContactPage.vue
@@ -1,37 +1,57 @@
 <script>
+import axios from 'axios';
+
+const endpoint = 'http://localhost:8000/api/contact-message';
+const emptyForm = {email: '', subject: '', message: ''};
 
 export default {
   name: 'ContactPage',
   data() {
-    return {}
+    return {
+      form: emptyForm
+    }
   },
   components: {},
   props: {},
   computed: {},
-  methods: {}
+  methods: {
+    sendForm(){
+      axios.post(endpoint, this.form)
+      .then(res => {
+        this.form = emptyForm;
+      })
+      .catch(err => {
+        console.error(err);
+      })
+      .then(() => {
+
+      })
+    }
+  }
 }
 </script>
 
 <template>
   <h1 class="py-3">Contact Us</h1>
 
-  <form>
+  <form @submit.prevent="sendForm">
     <!-- Email -->
     <div class="mb-3">
       <label for="email" class="form-label">
         <span>Your Email address</span>
         <span><sup class="text-danger">*</sup></span>
       </label>
-      <input type="email" class="form-control" id="email" placeholder="your-email@mail.com">
+      <input type="email" class="form-control" id="email" placeholder="your-email@mail.com" v-model.trim="form.email">
+      <small class="form-text text-muted">Your email. We will contact you at this email.</small>
     </div>
 
-    <!-- Object -->
+    <!-- Subject -->
     <div class="mb-3">
-      <label for="object" class="form-label">
-        <span>Email object</span>
+      <label for="subject" class="form-label">
+        <span>Email subject</span>
         <span><sup class="text-danger">*</sup></span>
       </label>
-      <input type="text" class="form-control" id="object" placeholder="Subject">
+      <input type="text" class="form-control" id="subject" placeholder="Subject" v-model.trim="form.subject">
     </div>
 
     <!-- Message -->
@@ -40,13 +60,20 @@ export default {
         <span>Write your message</span>
         <span><sup class="text-danger">*</sup></span>
       </label>
-      <textarea class="form-control" id="message" rows="3"></textarea>
+      <textarea class="form-control" id="message" rows="3" v-model.trim="form.message"></textarea>
     </div>
+
+    <div class="mb-3">
+      <button type="submit" class="btn btn-primary">Send</button>
+    </div>
+
   </form>
 </template>
 
 <style lang="scss" scoped>
-form>*{
+form{
+  max-width: 1280px;
+  min-width: 720px;
   text-align: left;
 }
 </style>
